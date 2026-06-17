d32d295f9f7cb90a7783b5604a27e2f862b7d087
karankoradiya <karan.koradiya@azilen.com>
2026-06-18 01:38:08 +0530

    add Swagger UI and OpenAPI config to both services

---
+++ task-service/src/main/java/com/example/task/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.example.task.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Task Service API", version = "1.0", description = "Documentation for Task Service API"),
+        servers = @Server(url = "http://localhost:8082", description = "Local Environment"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
